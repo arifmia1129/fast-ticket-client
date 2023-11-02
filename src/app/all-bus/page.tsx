53d749da158667f41fd697e9ffb8109878cc9edf
arifmia1129 <arifmia1129@gmail.com>
2023-11-02 19:20:40 +0600

    bus review style changed

--- src/app/all-bus/page.tsx
+++ src/app/all-bus/page.tsx
@@ -6,6 +6,7 @@ import { primaryColor } from "@/utils/color";
 import { Button, Card, Row } from "antd";
 import { useState } from "react";
 import { MessageOutlined, UserOutlined, StarOutlined } from "@ant-design/icons";
+import Marquee from "react-fast-marquee";
 
 export default function AllBus() {
   const { data } = useGetBusQuery({ limit: 300 });
@@ -44,26 +45,29 @@ export default function AllBus() {
                   }
                   style={{ width: "95vw", margin: "10px" }}
                 >
-                  {busId === bus._id &&
-                    bus?.review?.map((r: any) => (
-                      <div key={r._id}>
-                        <Row>
-                          <UserOutlined />
-                          <p style={{ fontWeight: "bold", margin: "0 5px" }}>
-                            {r.name}
-                          </p>
-                        </Row>
-                        <Row>
-                          <MessageOutlined />
-                          <p style={{ margin: "0 5px" }}>{r.review}</p>
-                        </Row>
-                        <Row>
-                          <StarOutlined />
-                          <p style={{ margin: "0 5px" }}>{r.rating} out of 5</p>
-                        </Row>
-                        <hr />
-                      </div>
-                    ))}
+                  <Marquee gradient={false}>
+                    {busId === bus._id &&
+                      bus?.review?.map((r: any) => (
+                        <div style={{ margin: "0 20px" }} key={r._id}>
+                          <Row>
+                            <UserOutlined />
+                            <p style={{ fontWeight: "bold", margin: "0 5px" }}>
+                              {r.name}
+                            </p>
+                          </Row>
+                          <Row>
+                            <MessageOutlined />
+                            <p style={{ margin: "0 5px" }}>{r.review}</p>
+                          </Row>
+                          <Row>
+                            <StarOutlined />
+                            <p style={{ margin: "0 5px" }}>
+                              {r.rating} out of 5
+                            </p>
+                          </Row>
+                        </div>
+                      ))}
+                  </Marquee>
                 </Card>
               </div>
             ))}
